1f9ec23a71a245b5259e35709d237baa0246e109
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-12-26 11:51:06 +0300

    added thoughts model

---
+++ backend/prisma/migrations/20251226085005_add_thought_mvp/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - Added the required column `name` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "name" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Thought" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "source" TEXT NOT NULL,
+    "topic" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Thought_pkey" PRIMARY KEY ("id")
+);
